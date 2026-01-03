c50f4adf79cd13652772c38043fbbd838ed78985
Adnan <adnanslametwibowo@gmail.com>
2026-01-03 13:13:57 +0700

    Add files via upload

---
+++ api/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+
+# from flask_talisman import Talisman
+
+app = Flask(__name__, template_folder='../templates', static_folder='../static')
+
+# Talisman(app, 
+#     content_security_policy=None,
+#     force_https=False
+# )
+
+@app.route('/')
+def home():
+    return render_template('main.html')
+
+# @app.route('/')
+# def Maintenance_page():
+#     return render_template('Maintenance.html')
+
+@app.route('/blogspot')
+def blog():
+    return render_template('blogSOON.html')
+
+if __name__ == '__main__':
+    app.run
