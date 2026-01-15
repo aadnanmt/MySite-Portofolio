70411cd6bfc36c5c1dd345ce079719f1919c812e
Adnan Slamet Wibowo <contact@aadnanmt.web.id>
2026-01-15 13:04:46 +0000

    chore: update dependencies & remove talisman

--- api/app.py
+++ api/app.py
@@ -1,25 +1,20 @@
 from flask import Flask, render_template, request
-# from flask_talisman import Talisman
 from flask_minify import Minify
+from flask_minify import Minify, decorators as minify_decorators
 
 app = Flask(__name__, template_folder='../templates', static_folder='../static')
 
-Minify(app=app, html=True, js=True, cssless=True)
-
-# Talisman(app, 
-#     content_security_policy=None,
-#     force_https=False
-# )
-
 @app.route('/')
+@minify_decorators.minify(html=True, js=True, cssless=True, caching_limit=0)
 def home():
     return render_template('main.html')
 
 # @app.route('/')
 # def Maintenance_page():
-#     return render_template('Maintenance.html')
+#     return render_template('maintenance.html')
 
 @app.route('/blog')
+@minify_decorators.minify(html=True, js=True, cssless=True, caching_limit=0)
 def blog():
     return render_template('404.html')
 
