796ed8c1299d3c455104985f5d97c8b0f4d2d169
Adnan <aadnanmtcontact@gmail.com>
2026-01-16 08:09:55 +0000

    chore: add caching limit for minify file html, css, and js

--- api/app.py
+++ api/app.py
@@ -3,10 +3,14 @@ from flask_minify import Minify
 
 app = Flask(__name__, template_folder='../templates', static_folder='../static')
 
-Minify(app=app, html=True, js=True, cssless=True)
+Minify(app=app, html=True, js=True, cssless=True, caching_limit=1000)
 
 @app.after_request
 def add_header(response):
+    if request.path.startswith('/static'):
+        response.headers['Cache-Control'] = 'public, max-age=31536000'
+    else:
+        response.headers['Cache-Control'] = 'public, max-age=0'
     response.headers['X-Content-Type-Options'] = 'nosniff'
     response.headers['X-Frame-Options'] = 'DENY'
     response.headers['X-XSS-Protection'] = '1; mode=block'
