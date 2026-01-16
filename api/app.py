a681207a181d6fdae0f62e994f992506e2bb323e
Adnan Slamet Wibowo <contact@aadnanmt.web.id>
2026-01-16 06:31:31 +0000

    chore: disable/delete header on vercel.json

--- api/app.py
+++ api/app.py
@@ -4,6 +4,13 @@ from flask_minify import Minify, decorators as minify_decorators
 
 app = Flask(__name__, template_folder='../templates', static_folder='../static')
 
+@app.after_request
+def add_header(response):
+    response.headers['X-Content-Type-Options'] = 'nosniff'
+    response.headers['X-Frame-Options'] = 'DENY'
+    response.headers['X-XSS-Protection'] = '1; mode=block'
+    return response
+
 @app.route('/')
 @minify_decorators.minify(html=True, js=True, cssless=True, caching_limit=0)
 def home():
