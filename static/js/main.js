c6029c0e9b6e80dd3230a81aae1f9da83177bcee
Adnan <aadnanmtcontact@gmail.com>
2026-01-16 12:05:47 +0000

    feat: go to top {hero} section

--- static/js/main.js
+++ static/js/main.js
@@ -299,4 +299,22 @@ document.addEventListener("DOMContentLoaded", () => {
     // ==== DATE TO COPYRIGHT FOOTER
     const yearElement = document.getElementById("current-year");
     if (yearElement) yearElement.textContent = new Date().getFullYear();
+
+        const toTopButton = document.querySelector('.top');
+
+        window.addEventListener('scroll', function() {
+            const scrollHeight = document.documentElement.scrollHeight;
+            const clientHeight = document.documentElement.clientHeight;
+            const scrollTop = window.scrollY;
+
+            const triggerPoint = (scrollHeight - clientHeight) * 0.88;
+
+            if (scrollTop > triggerPoint) {
+                
+                toTopButton.classList.add('show');
+            } else {
+                
+                toTopButton.classList.remove('show');
+            }
+        });
 });
