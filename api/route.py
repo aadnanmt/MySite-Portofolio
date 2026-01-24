f8d25b6543fa4074ef1249a3abe0f798abd9d6af
Adnan <aadnanmtcontact@gmail.com>
2026-01-24 14:08:35 +0700

    feat: enable MVC method too, before app.py only to 3 {*.py}

---
+++ api/route.py
@@ -0,0 +1,38 @@
+# api/route.py
+from flask import Blueprint, render_template, current_app
+from utility import get_posts_cached
+import markdown
+import os
+import frontmatter
+
+# make blueprint
+main_bp = Blueprint('main', __name__)
+
+# app.route --> main_bp.route
+@main_bp.route('/')
+def home():
+    return render_template('main.html')
+
+# blog page
+@main_bp.route('/blog')
+def blog_index():
+    posts = get_posts_cached(current_app.root_path) 
+    return render_template('blog_index.html', posts=posts)
+
+@main_bp.route('/blog/<slug>')
+def blog_post(slug):
+    post_folder = os.path.join(current_app.root_path, 'posts') 
+    filepath = os.path.join(post_folder, f'{slug}.md')
+    
+    if not os.path.exists(filepath):
+        return render_template('404.html')
+        
+    with open(filepath, 'r', encoding='utf-8') as f:
+        post = frontmatter.load(f)
+        content = markdown.markdown(
+            post.content, 
+            extensions=['fenced_code', 'codehilite', 'tables', 'attr_list']
+        )
+        
+    return render_template('blog_post.html', post=post, content=content)
+    pass
