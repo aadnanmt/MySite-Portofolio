7816a48d38999ce2c81815ad289ba7b00e4fcb32
Adnan Slamet Wibowo <contact@aadnanmt.web.id>
2026-02-05 06:31:20 +0700

    fix(security): correct indentation scope for return response

--- api/security/headers.py
+++ api/security/headers.py
@@ -1,14 +1,15 @@
 from flask import request
 
 def security_headers(app):
-
-	@app.after_request
-	def add_header(response):
-    	if request.path.startswith('/static'):
-        	response.headers['Cache-Control'] = 'public, max-age=31536000'
-    	else:
-        	response.headers['Cache-Control'] = 'public, max-age=0'
-        	response.headers['X-Content-Type-Options'] = 'nosniff'
-        	response.headers['X-Frame-Options'] = 'DENY'
-        	response.headers['X-XSS-Protection'] = '1; mode=block'
-    return response
+    
+    @app.after_request
+    def add_header(response):
+        if request.path.startswith('/static'):
+            response.headers['Cache-Control'] = 'public, max-age=31536000'
+        else:
+            response.headers['Cache-Control'] = 'public, max-age=0'
+            response.headers['X-Content-Type-Options'] = 'nosniff'
+            response.headers['X-Frame-Options'] = 'DENY'
+            response.headers['X-XSS-Protection'] = '1; mode=block'
+            
+        return response
