5727f5a35f544fd9d62389a4bd189b4045384e3d
Adnan <aadnanmtcontact@gmail.com>
2026-02-05 06:15:11 +0700

    refactor: migrate entry point to root app.py and cleanup legacy api files

---
+++ app.py
@@ -0,0 +1,25 @@
+# app.py
+import os
+import sys
+
+# change to absolute path
+current_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(current_dir)
+
+from flask import Flask, request
+from flask_minify import Minify
+
+from api.security.headers import security_headers
+from api,routes.home import main_bp
+
+template_dir = os.path.join(current_dir, 'templates')
+static_dir = os.path.join(current_dir, 'static')
+
+app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
+
+Minify(app=app, html=True, js=True, cssless=True)
+
+app.register_blueprint(main_bp)
+
+if __name__ == '__main__':
+    app.run
