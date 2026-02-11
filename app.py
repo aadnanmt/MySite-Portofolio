0d8d80d3f3f0d4f2bb37f68507f0799c9dbdc7e0
Adnan <aadnanmtcontact@gmail.com>
2026-02-11 21:43:01 +0700

    rename: variable current_dir to current_directory and static_dir to static_directory on entry point pyton

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import os
 import sys
 
 # change to absolute path
-current_dir = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(current_dir)
+current_directory = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(current_directory)
 
 from flask import Flask, request
 from flask_minify import Minify
@@ -13,10 +13,10 @@ from api.security.headers import security_headers
 from api.routes.home import main_bp
 from api.routes.blog import blog_bp
 
-template_directory = os.path.join(current_dir, 'templates')
-static_dir = os.path.join(current_dir, 'static')
+template_directory = os.path.join(current_directory, 'templates')
+static_directory = os.path.join(current_directory, 'static')
 
-app = Flask(__name__, template_folder=template_directory, static_folder=static_dir)
+app = Flask(__name__, template_folder=template_directory, static_folder=static_directory)
 
 Minify(app=app, html=True, js=True, cssless=True)
 
