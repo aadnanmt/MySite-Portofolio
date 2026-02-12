cf727a1ed1c4228cc09dd5876f095e4028c1afb1
Adnan <aadnanmtcontact@gmail.com>
2026-02-12 13:07:46 +0700

    chore(app.py): add () on app.run => app.run() for looping server flask and import vite_app/init_vite

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from flask_minify import Minify
 from api.security.headers import security_headers
 from api.routes.home import main_bp
 from api.routes.blog import blog_bp
+from api.utility.vite_app import init_vite
 
 template_directory = os.path.join(current_directory, 'templates')
 static_directory = os.path.join(current_directory, 'static')
@@ -20,8 +21,10 @@ app = Flask(__name__, template_folder=template_directory, static_folder=static_d
 
 Minify(app=app, html=True, js=True, cssless=True)
 
+init_vite(app)
+
 app.register_blueprint(main_bp)
 app.register_blueprint(blog_bp)
 
 if __name__ == '__main__':
-    app.run
+    app.run()
