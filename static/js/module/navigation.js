bfa52cb58759090e98d5962c13e210cf5247fad3
aadnanmt <aadnanmtcontact@gmail.com>
2026-03-02 13:45:42 +0700

    chore: disable my preloader

--- static/js/module/navigation.js
+++ static/js/module/navigation.js
@@ -30,3 +30,11 @@ export const initNavigation = () => {
     });
   }
 };
+
+const hash = window.location.hash;
+   if (hash) {
+     const el = document.querySelector(hash);
+     if (el) {
+       setTimeout(() => el.scrollIntoView({ behavior: 'smooth' }), 100);
+     }
+   }
