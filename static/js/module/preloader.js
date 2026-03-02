5afaa4bce67c1956b7a003f2469fee246aa3eb74
aadnanmt <aadnanmtcontact@gmail.com>
2026-03-02 13:44:44 +0700

    chore: disable my preloader

--- static/js/module/preloader.js
+++ static/js/module/preloader.js
@@ -1,16 +1,16 @@
-// frontend/module/preloader.js
+// // frontend/module/preloader.js
 
-// export preloader function
-export const initPreloader = () => {
-  const preloader = document.getElementById("preloader");
-  if (!preloader) return;
+// // export preloader function
+// export const initPreloader = () => {
+//   const preloader = document.getElementById("preloader");
+//   if (!preloader) return;
 
-  window.addEventListener("load", () => {
-    setTimeout(() => {
-      preloader.style.opacity = "0";
-      setTimeout(() => {
-        preloader.style.display = "none";
-      }, 350);
-    }, 700);
-  });
-};
+//   window.addEventListener("load", () => {
+//     setTimeout(() => {
+//       preloader.style.opacity = "0";
+//       setTimeout(() => {
+//         preloader.style.display = "none";
+//       }, 350);
+//     }, 700);
+//   });
+// };
