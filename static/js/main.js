8dc9e9e023ca16af5711857bf30e0eff2ac813da
aadnanmt <aadnanmtcontact@gmail.com>
2026-03-04 10:48:24 +0700

    remove: particle module import and init from main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,23 +1,19 @@
 // frontend/js/main.js
 
 // import module
-// import { initPreloader } from './module/preloader.js';
 import { initNavigation } from './module/navigation.js';
 import { LanguageManager } from './module/language.js';
 import { initTyping } from './module/typing.js';
 import { initComponents } from './module/ui.js';
-import { initCopyButton } from './module/util.js'; 
-import { initParticle } from './module/particle.js';
+import { initCopyButton } from './module/util.js';
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("JS okeyyy...");
 
   // execute module
-  // initPreloader();
   initNavigation();
   LanguageManager.init();
   initTyping();
   initComponents();
   initCopyButton();
-  initParticle();
 });
